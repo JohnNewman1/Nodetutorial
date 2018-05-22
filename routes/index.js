a56a5b9c8b0e769cfe27a46280edae806d1987f7
John Newman <newmanj903@gmail.com>
2018-05-22 14:32:22 +0100

    setting up database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var spaces = require('../server/models').spaces
+
+var dbtitle = spaces.findAll( {}
+  ).then(function(spaces){
+    spaces.forEach(function(space){
+      console.log(space.get('title'))
+    })
+
+  })
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('/index.html');
+  res.render('index', { title: 'Makers', description: dbtitle});
 });
 
 module.exports = router;
