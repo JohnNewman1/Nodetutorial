1bc79aaf5486e4e24ee987dcaf36ed51633f406c
John Newman <newmanj903@gmail.com>
2018-05-22 10:25:18 +0100

    database connected

--- models/database.js
+++ models/database.js
@@ -1,8 +1,12 @@
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+const Sequelize = require('sequelize');
 
-const client = new pg.Client(connectionString);
-client.connect();
-const query = client.query(
-  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', () => { client.end(); });
+const sequelize = new Sequelize('postgres://localhost:5432/todo');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
